15a329b240282ca3d3c11dabe3e33c0e58b3f23a
jayfont <jaydenfont@gmail.com>
2025-10-05 20:26:32 -0400

    Set CXX standard to 17

--- start_container.sh
+++ start_container.sh
@@ -3,8 +3,3 @@
 # Launch the devkitpro container, mounting the current directory (the WSL LibNX directory) to the home directory of the container
 
 sudo docker run  -v .:/home -i -t devkitpro/devkita64 /bin/bash
-
-
-# Tips
-- Do not create any files in the Docker container via terminal commands, this prevents VSCode from modifying them. Instead, use VSCode
-  or your vanilla WSL2 terminal
